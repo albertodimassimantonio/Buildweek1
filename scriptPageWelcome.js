748089117261332507a2d5d0f9fa97df718ff336
AriannaDeSabata <168458400+AriannaDeSabata@users.noreply.github.com>
2024-12-10 19:24:25 +0100

    Create scriptPageWelcome.js

---
+++ scriptPageWelcome.js
@@ -0,0 +1,18 @@
+let check = document.getElementById("check")
+
+check.addEventListener("change", proceed)
+
+function proceed(){
+    if(check.checked){
+        let buttonProceed = document.getElementById("buttonProceed").disabled = false
+    }else{
+        buttonProceed.disabled = true
+    }
+}
+
+
+buttonProceed.addEventListener("click", loadQuiz)
+
+function loadQuiz(){
+    location.href ="#"
+}
